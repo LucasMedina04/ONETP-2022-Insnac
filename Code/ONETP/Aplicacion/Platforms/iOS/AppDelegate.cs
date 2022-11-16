ba0264384b3be48d00fca6cab3bed0676f153aa1
LucasMedina04 <medina.lucas.e@gmail.com>
2022-11-16 10:30:48 -0300

    a

--- Code/ONETP/Aplicacion/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Aplicacion;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
