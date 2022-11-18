6fc65e7c28ac6bab2aa09c0a5f24c883bee34606
Martín Morandini <84515114+MMorandiniGit@users.noreply.github.com>
2022-11-18 10:40:18 -0300

    TcP

---
+++ Code/Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using SuperSimpleTcp;
+using System.Text;
+
+
+// instantiate
+SimpleTcpServer server = new SimpleTcpServer("127.0.0.1:9000");
+
+    // set events
+    server.Events.ClientConnected += ClientConnected;
+    server.Events.ClientDisconnected += ClientDisconnected;
+    server.Events.DataReceived += DataReceived;
+
+    // let's go!
+    server.Start();
+
+    // once a client has connected...
+    server.Send("[ClientIp:Port]", "Hello, world!");
+    Console.ReadKey();
+
+
+static void ClientConnected(object sender, ConnectionEventArgs e)
+{
+    Console.WriteLine($"[{e.IpPort}] client connected");
+}
+
+static void ClientDisconnected(object sender, ConnectionEventArgs e)
+{
+    Console.WriteLine($"[{e.IpPort}] client disconnected: {e.Reason}");
+}
+
+static void DataReceived(object sender, DataReceivedEventArgs e)
+{
+    Console.WriteLine($"[{e.IpPort}]: {Encoding.UTF8.GetString(e.Data.Array, 0, e.Data.Count)}");
+}
